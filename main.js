d052715249014dfd58ca3855bafa6e5476b109f7
EliGarcia <garciaelisha93@yahoo.com>
2017-08-03 15:04:55 -0500

    all button display in output

--- main.js
+++ main.js
@@ -1,8 +1,74 @@
-
-
-document.getElementById("seven").addEventListener("click", function(event){
-
-
+document.getElementById("seven").addEventListener("click", function(event) {
   let input = `7`;
   document.querySelector('.display').innerHTML += input;
-  });
+});
+
+document.getElementById("eight").addEventListener("click", function(event) {
+  let input = `8`;
+  document.querySelector('.display').innerHTML += input;
+});
+
+document.getElementById("nine").addEventListener("click", function(event) {
+  let input = `9`;
+  document.querySelector('.display').innerHTML += input;
+});
+
+document.getElementById("four").addEventListener("click", function(event) {
+  let input = `4`;
+  document.querySelector('.display').innerHTML += input;
+});
+
+document.getElementById("five").addEventListener("click", function(event) {
+  let input = `5`;
+  document.querySelector('.display').innerHTML += input;
+});
+
+document.getElementById("six").addEventListener("click", function(event) {
+  let input = `6`;
+  document.querySelector('.display').innerHTML += input;
+});
+
+document.getElementById("one").addEventListener("click", function(event) {
+  let input = `1`;
+  document.querySelector('.display').innerHTML += input;
+});
+
+document.getElementById("two").addEventListener("click", function(event) {
+  let input = `2`;
+  document.querySelector('.display').innerHTML += input;
+});
+
+document.getElementById("three").addEventListener("click", function(event) {
+  let input = `3`;
+  document.querySelector('.display').innerHTML += input;
+});
+
+document.getElementById("zero").addEventListener("click", function(event) {
+  let input = `0`;
+  document.querySelector('.display').innerHTML += input;
+});
+
+document.getElementById("divide").addEventListener("click", function(event) {
+  let input = `/`;
+  document.querySelector('.display').innerHTML += input;
+});
+
+document.getElementById("multiply").addEventListener("click", function(event) {
+  let input = `*`;
+  document.querySelector('.display').innerHTML += input;
+});
+
+document.getElementById("addition").addEventListener("click", function(event) {
+  let input = `+`;
+  document.querySelector('.display').innerHTML += input;
+});
+
+document.getElementById("subtract").addEventListener("click", function(event) {
+  let input = `-`;
+  document.querySelector('.display').innerHTML += input;
+});
+
+document.getElementById("decimal").addEventListener("click", function(event) {
+  let input = `.`;
+  document.querySelector('.display').innerHTML += input;
+});
